bb5d997ea5a2c0812cd0005ae5d15ee4606cdc13
AbroShahzeb <khizr772@gmail.com>
2025-04-21 15:02:44 +0500

    Initial commit

---
+++ .scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+
+# Make sure NVM is available
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+# Installing Dependencies
+echo "Installing Dependencies..."
+npm install --yes
+
+# Creating a build 
+echo "Building application"
+npm run build
+
+
+# Restarting server
+echo "Restarting server"
+pm2 restart finconnect-backend
+
+echo "Deployment Finished!"
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+configDotenv();
+import "colors";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.json({
+    status: "success",
+    message: "Hello from the finconnect backend API.",
+  });
+});
+
+app.listen(process.env.PORT || 3007, () => {
+  console.log(
+    `App is running on PORT ${process.env.PORT || 3007}`.yellow.underline
+  );
+});
